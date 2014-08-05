f94bc06d9bcf01a20497ba5f9dbcb44fdd186c58
Anthony Seure <anthony.seure@gmail.com>
2014-08-05 16:25:12 +0200

    Fix the deploy.sh script

--- deploy.sh
+++ deploy.sh
@@ -1,30 +1,34 @@
 #! /bin/bash
 
 # Set those variables to fit your configuration
-EPITA_LOGIN='login'
-EPTIA_PASS_SOCKS='password'
-COLOR_ONE='blue'
-COLOR_TWO='yellow'
+EPITA_LOGIN="seure-_a"
+EPITA_PASS_SOCKS="toto"
+COLOR_ONE="green"
+COLOR_TWO="red"
 
 # Modify SOUL configuration
-sed 's/login_x/$LOGIN/' .jogsoul/jogsoul.conf > .jogsoul_tmp
-sed 's/password/$PASSWORD/' .jogsoul_tmp > .jogsoul/jogsoul.conf
+sed "s/login_x/$EPITA_LOGIN/" .jogsoul/jogsoul.conf > .jogsoul_tmp
+sed "s/password/$EPITA_PASS_SOCKS/" .jogsoul_tmp > .jogsoul/jogsoul.conf
 rm .jogsoul_tmp
 
 # Modify TMUX configuration
-sed 's/blue/$COLOR_ONE/' .tmux.conf > .tmux.conf_tmp
+sed "s/blue/$COLOR_ONE/" .tmux.conf > .tmux.conf_tmp
 mv .tmux.conf_tmp .tmux.conf
 
 # Modify ZSH configuration
-sed 's/fg[blue]/fg[$COLOR_ONE]/' .zshrc > .zshrc_tmp
-sed 's/fg[yellow]/fg[$COLOR_TWO]/' .zshrc_tmp > .zshrc
+sed "s/fg\[blue\]/fg[$COLOR_ONE]/" .zshrc > .zshrc_tmp
+sed "s/fg\[yellow\]/fg[$COLOR_TWO]/" .zshrc_tmp > .zshrc
 rm .zshrc_tmp
 
+# Prepare Vim
+mkdir -p .vim/bundle
+git clone https://github.com/gmarik/Vundle.vim .vim/bundle/Vundle.vim
+vim +BundleInstall +qa!
 
 # Mac specific
 PLATFORM=`uname`
-if [[ $platform == 'Darwin' ]]; then
-  sed 's/ls --color=auto/ls -G/' .zshrc > .zshrc_tmp
+if [[ $platform == "Darwin" ]]; then
+  sed "s/ls --color=auto/ls -G/" .zshrc > .zshrc_tmp
   mv .zshrc_tmp .zshrc
 fi
 
