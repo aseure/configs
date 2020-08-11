d7192da98a5f5e3f62eed2d09cf785091fdf9b14
Anthony Seure <anthony.seure@gmail.com>
2020-08-11 09:23:41 +0200

    deploy: simplify and fix deploy script

--- deploy.sh
+++ deploy.sh
@@ -4,23 +4,12 @@ echo "> Current configuration files for Git, Vim and ZSH will be overriden."
 echo "> Press any key to continue or press ^C to abort"
 read
 
-# Set those variables to fit your configuration
-GIT_CONFIG_NAME="Anthony Seure"
-GIT_CONFIG_EMAIL="anthony.seure@gmail.com"
+git reset --hard HEAD
 
 # Mac specific
 PLATFORM=`uname`
-
-git reset --hard HEAD
-
-# Modify Git configuration
 if [[ $PLATFORM == "Darwin" ]]; then
-  sed -i '' "s/Anthony Seure/$GIT_CONFIG_NAME/g"              .gitconfig
-  sed -i '' "s/anthony\.seure@gmail\.com/$GIT_CONFIG_EMAIL/g" .gitconfig
   defaults write com.jetbrains.intellij ApplePressAndHoldEnabled -bool false
-else
-  sed -i    "s/Anthony Seure/$GIT_CONFIG_NAME/g"              .gitconfig
-  sed -i    "s/anthony\.seure@gmail\.com/$GIT_CONFIG_EMAIL/g" .gitconfig
 fi
 
 # Reinstall Vim Plug
@@ -32,13 +21,11 @@ rm -rf .zfunctions/pure
 git clone git@github.com:sindresorhus/pure.git .zfunctions/pure
 
 # Create relatives links
-rm -f ~/.gitconfig        && ln -s $PWD/.gitconfig ~
-rm -f ~/.gitignore_global && ln -s $PWD/.gitignore_global ~
-rm -f ~/.vim*             && ln -s $PWD/.vim* ~
-rm -f ~/.zshrc            && ln -s $PWD/.zshrc ~
-rm -f ~/.zfunctions       && ln -s $PWD/.zfunctions ~
-ln -s $PWD/pure/pure.zsh  ~/.zfunctions/pure/prompt_pure_setup
-ln -s $PWD/pure/async.zsh ~/.zfunctions/pure/async
+rm -f  ~/.gitconfig        && ln -s $PWD/.gitconfig ~
+rm -f  ~/.gitignore_global && ln -s $PWD/.gitignore_global ~
+rm -rf ~/.vim*             && ln -s $PWD/.vim* ~
+rm -f  ~/.zshrc            && ln -s $PWD/.zshrc ~
+rm -rf ~/.zfunctions       && ln -s $PWD/.zfunctions ~
 
 cd
 vim +PlugInstall +qa!
