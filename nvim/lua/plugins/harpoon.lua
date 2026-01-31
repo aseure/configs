8d64bd0cd316edae42e4184cb2c0ef99512e9a96
Anthony Seure <anthony.seure@gmail.com>
2026-01-31 14:54:12 +0100

    nvim: give a try to Harpoon

---
+++ nvim/lua/plugins/harpoon.lua
@@ -0,0 +1,64 @@
+return {
+	"ThePrimeagen/harpoon",
+	branch = "harpoon2",
+	dependencies = { "nvim-lua/plenary.nvim" },
+	config = function()
+		require("harpoon"):setup()
+	end,
+	keys = {
+		{
+			"<leader>hh",
+			function()
+				require("harpoon").ui:toggle_quick_menu(require("harpoon"):list())
+			end,
+		},
+		{
+			"<leader>h1",
+			function()
+				require("harpoon"):list():replace_at(1)
+			end,
+		},
+		{
+			"<leader>h2",
+			function()
+				require("harpoon"):list():replace_at(2)
+			end,
+		},
+		{
+			"<leader>h3",
+			function()
+				require("harpoon"):list():replace_at(3)
+			end,
+		},
+		{
+			"<leader>h4",
+			function()
+				require("harpoon"):list():replace_at(4)
+			end,
+		},
+		{
+			"<leader>1",
+			function()
+				require("harpoon"):list():select(1)
+			end,
+		},
+		{
+			"<leader>2",
+			function()
+				require("harpoon"):list():select(2)
+			end,
+		},
+		{
+			"<leader>3",
+			function()
+				require("harpoon"):list():select(3)
+			end,
+		},
+		{
+			"<leader>4",
+			function()
+				require("harpoon"):list():select(4)
+			end,
+		},
+	},
+}
