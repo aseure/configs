fbef83a6fea393889d634dba885f4a38e6ea6df0
Anthony Seure <anthony.seure@gmail.com>
2026-02-19 15:05:13 +0100

    nvim: add <leader>gd to toggle git diff

--- nvim/lua/plugins/git.lua
+++ nvim/lua/plugins/git.lua
@@ -10,6 +10,22 @@ return {
 					require("gitsigns").blame()
 				end,
 			},
+			{
+				"<leader>gd",
+				function()
+					if vim.wo.diff then
+						for _, win in ipairs(vim.api.nvim_list_wins()) do
+							local buf = vim.api.nvim_win_get_buf(win)
+							if vim.api.nvim_buf_get_name(buf):match("^gitsigns://") then
+								vim.api.nvim_win_close(win, false)
+								return
+							end
+						end
+					else
+						require("gitsigns").diffthis()
+					end
+				end,
+			},
 		},
 	},
 	{
