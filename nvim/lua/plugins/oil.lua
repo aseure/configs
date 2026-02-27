9e6382821f7cb96023be5867e16ca93a3531d21d
Anthony Seure <anthony.seure@gmail.com>
2026-02-27 12:15:00 +0100

    nvim: show hidden files by default in oil

--- nvim/lua/plugins/oil.lua
+++ nvim/lua/plugins/oil.lua
@@ -3,6 +3,9 @@ return {
 		"stevearc/oil.nvim",
 		opts = {
 			skip_confirm_for_simple_edits = true,
+			view_options = {
+				show_hidden = true,
+			},
 		},
 		keys = {
 			{ "-", "<CMD>Oil<CR>", desc = "Open parent directory" },
